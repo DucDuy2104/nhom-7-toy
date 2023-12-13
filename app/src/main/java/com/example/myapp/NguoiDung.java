77b8bca6bc683ad8cf271852bb75afe92fc74790
Van Nguyen <nguyenvo311202@gmail.com>
2023-12-14 01:15:23 +0700

    Van Nguyen

--- app/src/main/java/com/example/myapp/NguoiDung.java
+++ app/src/main/java/com/example/myapp/NguoiDung.java
@@ -1,14 +1,7 @@
 package com.example.myapp;
 
 public class NguoiDung {
-lic void setDiaChiNd(String diaChiNd) {
-        this.diaChiNd = diaChiNd;
-    }
- String getImageUri() {
-        return imageUri;
-    }
-
-    public void setImageUri(String imageUri) {
+lic vic void setImageUri(String imageUri) {
         this.imageUri = imageUri;
     }
 }
