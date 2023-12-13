260cd4026b9dbf14e7fb6a00b21c1f3cc477a90f
Van Nguyen <nguyenvo311202@gmail.com>
2023-12-14 01:08:38 +0700

    Van Nguyen

--- app/src/main/java/com/example/myapp/NguoiDung.java
+++ app/src/main/java/com/example/myapp/NguoiDung.java
@@ -4,12 +4,7 @@ public class NguoiDung {
     p dùng, giới tính, ngày sinh, số điện thoại, email, dịa chỉ, tên đăng nhập, mật khẩu, đường dẫn ảnh
     private int role;
 
-    public NguoiDung(String tenNd, String gioiTinhNd, String ngaySinhNd, String sdtNd, String emailNd, String diaChiNd, String tenDn, String matKhau, String imageUri, int role) {
-        this.tenNd = tenNd;
-        this.gioiTinhNd = gioiTinhNd;
-        this.ngaySinhNd = ngaySinhNd;
-        this.sdtNd = sdtNd;
-        this.emailNd = emailNd;
+    publ emailNd;
         this.diaChiNd = diaChiNd;
         this.tenDn = tenDn;
         this.matKhau = matKhau;
