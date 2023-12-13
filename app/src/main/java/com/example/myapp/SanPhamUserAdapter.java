f771e7a878f8e4906d4e711299503fa46851da82
Van Tai <tai2003e@gmail.com>
2023-12-13 23:48:15 +0700

    Van Tai

--- app/src/main/java/com/example/myapp/SanPhamUserAdapter.java
+++ app/src/main/java/com/example/myapp/SanPhamUserAdapter.java
@@ -1,4 +1,15 @@
 package com.example.myapp;
 
+import android.content.Context;
+
+import java.util.List;
+
 public class SanPhamUserAdapter {
+    Context context;
+    List moHinhList;
+
+    public SanPhamUserAdapter(Context context, List moHinhList) {
+        this.context = context;
+        this.moHinhList = moHinhList;
+    }
 }
