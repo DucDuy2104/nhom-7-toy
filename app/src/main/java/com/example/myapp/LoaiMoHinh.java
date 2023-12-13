4bb46758d47afb2c1c87266c259abae15981388e
Van Nguyen <nguyenvo311202@gmail.com>
2023-12-14 01:18:27 +0700

    Van Nguyen

--- app/src/main/java/com/example/myapp/LoaiMoHinh.java
+++ app/src/main/java/com/example/myapp/LoaiMoHinh.java
@@ -1,11 +1,4 @@
 package com.example.myapp;
 
 public class LoaiMoHinh {
-    c StString getImgUri() {
-        return imgUri;
-    }
 
-    public void setImgUri(String imgUri) {
-        this.imgUri = imgUri;
-    }
-}
