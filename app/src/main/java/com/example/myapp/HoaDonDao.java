ea87b0f80a24ed06c43e0bf897f5ebb47bc29b53
Cao Vu Duc Duy <cvducduy@gmail.com>
2023-12-14 10:55:51 +0700

    Cao Vu Duc Duy

--- app/src/main/java/com/example/myapp/HoaDonDao.java
+++ app/src/main/java/com/example/myapp/HoaDonDao.java
@@ -21,13 +21,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class HoaDonDao {
-    public  interface  OnGetListHoaDon {
-        void onGetSuccess(List<HoaDon> hoaDonList);
-    }
 
-    public interface  OnGetHoaDon {
-        void onGetSuccess(HoaDon hoaDon);
-    }
 
     public interface OnGetHoaDonListByStatus {
         void onGetSuccess(List<HoaDon> listHoaDonOnGoing, List<HoaDon> listHoaDonGone, List<HoaDon> listHoaDonCancel);
