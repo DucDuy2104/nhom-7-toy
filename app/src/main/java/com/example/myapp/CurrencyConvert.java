0294177ddca77d0cad6e03bf545d39f80b4d62db
Cao Vu Duc Duy <cvducduy@gmail.com>
2023-12-14 11:53:13 +0700

    Cao Vu Duc Duy

--- app/src/main/java/com/example/myapp/CurrencyConvert.java
+++ app/src/main/java/com/example/myapp/CurrencyConvert.java
@@ -4,14 +4,7 @@ import java.text.NumberFormat;
 import java.util.Locale;
 
 public class CurrencyConvert {
-    public static String convertFromFloatToVNCurrency(int price) {
-        Locale localeVN = new Locale("vi", "VN");
-        NumberFormat currencyFormatter = NumberFormat.getCurrencyInstance(localeVN);
 
-        // Định dạng số tiền theo định dạng tiền tệ Việt Nam
-        String formattedAmount = currencyFormatter.format(price);
-        return formattedAmount;
-    }
 
     public static int converFromVNCurrencyToFloat(String formattedAmount) {
         String numericString = formattedAmount.replaceAll("[^\\d]", "");
